5f12cb6ef15decf06c575e8d1dc5ee98671337a4
iMateshuk <80713398+iMateshuk@users.noreply.github.com>
2021-04-02 20:44:21 +0300

    Add files via upload

---
+++ src/by/htp/les10/presentation/FractionView.java
@@ -0,0 +1,11 @@
+package by.htp.les10.presentation;
+
+import by.htp.les10.entity.Fraction;
+
+public class FractionView {
+	
+	public void printSum(Fraction f1, Fraction f2, Fraction f3) {
+		System.out.println(f1.getNumerator() + "/" + f1.getDenominator() + " + " + f2.getNumerator() + "/" + f2.getDenominator() + " = "+ f3.getNumerator() + "/" + f3.getDenominator());
+	}
+
+}
